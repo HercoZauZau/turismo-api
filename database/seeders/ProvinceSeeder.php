cdba3e50be786af95c34ed68f86d6ec7bd20a8c9
miltonmaleiane <miltonmaleiane1@gmail.com>
2023-08-31 13:53:39 +0200

    Provincd Seeder Update

--- database/seeders/ProvinceSeeder.php
+++ database/seeders/ProvinceSeeder.php
@@ -33,37 +33,37 @@ class ProvinceSeeder extends Seeder
             [
                 'name' => 'Sofala',
                 'description' => 'Home to the port city of Beira, a major trade hub.',
-                'image_url' => ''
+                'image_url' => 'https://i.pinimg.com/564x/6a/b1/78/6ab178c034bd77a426eacf47962a1890.jpg'
             ],
             [
                 'name' => 'Manica',
                 'description' => 'Known for its agricultural and mining activities.',
-                'image_url' => ''
+                'image_url' => 'https://i.pinimg.com/564x/9a/d0/22/9ad022c8a0c687c7d497e1d38c039815.jpg'
             ],
             [
                 'name' => 'Tete',
                 'description' => 'Rich in mineral resources and scenic landscapes.',
-                'image_url' => ''
+                'image_url' => 'https://i.pinimg.com/564x/d8/01/f5/d801f5a6a92d1496d9a477b203c2570a.jpg'
             ],
             [
                 'name' => 'Zambezia',
                 'description' => 'One of the most populous provinces in Mozambique.',
-                'image_url' => ''
+                'image_url' => 'https://images.unsplash.com/photo-1625819397064-9c694a43dbba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80'
             ],
             [
                 'name' => 'Nampula',
                 'description' => 'Known for its diverse culture and historical sites.',
-                'image_url' => ''
+                'image_url' => 'https://i.pinimg.com/750x/c2/bf/bc/c2bfbc95b220a02d3513a654013da8e9.jpg'
             ],
             [
                 'name' => 'Cabo Delgado',
                 'description' => 'Rich in natural resources, including gas reserves.',
-                'image_url' => ''
+                'image_url' => 'https://i.pinimg.com/564x/eb/42/b0/eb42b00d0134afd8a6ed607336268888.jpg'
             ],
             [
                 'name' => 'Niassa',
                 'description' => 'Located in the northernmost part of the country.',
-                'image_url' => ''
+                'image_url' => 'https://i.pinimg.com/564x/33/12/54/331254c2af738eb3d0677158e570c578.jpg'
             ],
         ];
 
@@ -71,6 +71,7 @@ class ProvinceSeeder extends Seeder
             DB::table('provincias')->insert([
                 'name' => $province['name'],
                 'description' => $province['description'],
+                'image_url' => $province['image_url'],
             ]);
         }
     }
